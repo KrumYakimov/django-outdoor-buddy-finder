cfe19bd560c5e67861c334685a12c2016ea4e2d0
Krum Yakimov <krum.yakimov@gmail.com>
2024-12-11 01:44:47 +0200

    Your deployment message

--- outdoor_buddy/core/views.py
+++ outdoor_buddy/core/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 from django.views import generic as views
 
 
 class HomePageView(views.TemplateView):
     template_name = "core/home-page.html"
+
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
