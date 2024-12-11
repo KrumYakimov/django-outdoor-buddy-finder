515468ff86f237406f29a6c7a23efa3e3b7410f0
Krum Yakimov <krum.yakimov@gmail.com>
2024-12-11 18:58:50 +0200

    modified settings.py

--- outdoor_buddy/settings.py
+++ outdoor_buddy/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG")
 
 ALLOWED_HOSTS = [
     config("NGROK_ENDPOINT"),
@@ -182,7 +182,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 # SMTP Server Settings
 EMAIL_HOST = config("EMAIL_HOST")
 EMAIL_PORT = config("EMAIL_PORT")
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = config("EMAIL_USE_TLS")
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
