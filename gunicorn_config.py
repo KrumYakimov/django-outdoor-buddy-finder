8b974f1b6f6103febf45264829ed20bbf763bb41
Krum Yakimov <krum.yakimov@gmail.com>
2024-12-11 02:30:02 +0200

    refactor the gunicorn_config

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,24 +1,10 @@
-# gunicorn_config.py
-
 import multiprocessing
 
-# Number of worker processes (recommended formula)
+
 workers = multiprocessing.cpu_count() * 2 + 1
-
-# Bind to all interfaces on port 8000
 bind = "0.0.0.0:8000"
-
-# Worker timeout in seconds
 timeout = 120
-
-# Logging level
 loglevel = "info"
-
-# Enable access log
 accesslog = "-"
-
-# Enable error log
 errorlog = "-"
-
-# Preload the application to save memory
 preload_app = True
